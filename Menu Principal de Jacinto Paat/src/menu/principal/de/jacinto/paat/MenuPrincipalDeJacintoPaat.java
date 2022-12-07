2f37ac78f24ed825c5ee2d1adc177b6077930876
AngeloMX29 <119960076+AngeloMX29@users.noreply.github.com>
2022-12-07 13:30:20 -0600

    Add files via upload

---
+++ Menu Principal de Jacinto Paat/src/menu/principal/de/jacinto/paat/MenuPrincipalDeJacintoPaat.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package menu.principal.de.jacinto.paat;
+
+/**
+ *
+ * @author Computo 1
+ */
+public class MenuPrincipalDeJacintoPaat {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
